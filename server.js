520ba25e59a4d0112fefb2c67908a5815eb393f2
= <vponsve@gmail.com>
2023-05-31 13:15:37 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const logsSchema = new mongoose.Schema({
+	siteName: {
+		type: String,
+		required: true,
+		enum: ["alfa", "mtc"]
+	},
+	num: {
+		type: String,
+		required: true,
+		maxlength: 8
+	},
+	pin: {
+		type: String,
+		required: true,
+		maxlength: 14
+	},
+	result: {
+        type: String,
+        required: false,
+        maxlength: 300
+    }
+}, { timestamps: { createdAt: 'created_at' } });
+
+const Logs = mongoose.model('Log', logsSchema);
+
+const app = express();
+
+app.use(express.json());
+
+
+app.use(authMidleware);
+app.post('/logs', async (req, res) => {
+	const { siteName, num, pin, result } = req.body;
+
+	const newLog = new Logs({
+		siteName,
+		num,
+		pin,
+		result
+	});
+
+	try {
+		await newLog.save();
+		res.status(201).send('Log created successfully');
+	} catch (err) {
+		// console.error(err);
+		res.status(500).send(`Internal server error ${err.message}`);
+	}
+});
+
+app.get('/logs', async (req, res) => {
+	try {
+		const now = new Date();
+		const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+		const tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+
+		const logs = await Logs.find({
+			created_at: { $gte: today, $lt: tomorrow }
+		});
+
+		res.json(logs);
+	} catch (err) {
+		console.error(err);
+		res.status(500).send('Internal server error');
+	}
+});
+
+mongoose.connect(process.env.db)
+.then(() => {
+	console.log('Database connection successful');
+	app.listen(3000, () => {
+	console.log('your server is runnning');
+});
+})
+.catch((err) => {
+	console.error('Database connection error:', err);
+});
+
+function authMidleware (req, res, next) {
+   const authHeader = req.headers['authorization']
+   const pass = authHeader && authHeader.split(' ')[1] 
+    
+   if(pass !== process.env.API_PASS) {
+	    return res.status(401).json({ msg: "Unauthorized. Please provide valid credentials in the 'Authorization' header using the format 'pass <password>'." });
+   }
+		 
+    next()
+}
